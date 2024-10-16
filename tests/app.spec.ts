77837173e201db3fe4abca2ccb6a5f4fc3c2ef6c
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-10-16 16:24:47 +0300

    feat: create first e2e tests for app + jaqpot.org

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,18 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://app.jaqpot.org/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Jaqpot/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://jaqpot.org/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.getByRole('link', { name: 'Getting started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Getting started' })).toBeVisible();
 });
