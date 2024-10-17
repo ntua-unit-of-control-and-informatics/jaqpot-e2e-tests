8da589d60e4c1bafc006f2f9b645458bbf07980f
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-10-17 15:13:51 +0300

    fix: increase expect timeout to 10000 to avoid flaky tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -76,6 +76,10 @@ export default defineConfig({
     // },
   ],
 
+  expect: {
+    timeout: 10 * 1000,
+  },
+
   /* Run your local dev server before starting the tests */
   // webServer: {
   //   command: 'npm run start',
