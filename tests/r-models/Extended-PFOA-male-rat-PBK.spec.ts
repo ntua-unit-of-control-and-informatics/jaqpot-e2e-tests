8aadc82d41a293e9a9955da9daa84b1692ec304e
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-10-17 13:34:41 +0300

    test: r-model

---
+++ tests/r-models/Extended-PFOA-male-rat-PBK.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from '@playwright/test';
+import { login } from '../util/login';
+import { ModelFormData, predict } from '../util/predict';
+
+const formData: ModelFormData[] = [
+  { selector: '[name="BW"]', value: '1', type: 'input' },
+  { selector: '[name="admin.type"]', value: 'IV', type: 'select' },
+  { selector: '[name="admin.time"]', value: '1', type: 'input' },
+  { selector: '[name="admin.dose"]', value: '1', type: 'input' },
+  { selector: '[name="sim.start"]', value: '1', type: 'input' },
+  { selector: '[name="sim.end"]', value: '11', type: 'input' },
+  { selector: '[name="sim.step"]', value: '1', type: 'input' },
+];
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://app.jaqpot.org/dashboard/models/1730/description');
+  await page.getByRole('button', { name: 'Accept' }).click();
+});
+
+test('should allow prediction if logged in', async ({ page }) => {
+  await login(page);
+  await page.getByRole('tab', { name: 'Predict' }).click();
+
+  await predict(page, formData);
+
+  await expect(page.getByText('Success')).toBeVisible();
+});
