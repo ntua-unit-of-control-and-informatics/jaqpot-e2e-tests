03ee3493cd0b733ed2913a86c24ac277f142de7b
alarv <alarvfm@gmail.com>
2024-10-18 11:19:41 +0300

    feat: add test for logistic regression model

---
+++ tests/sklearn-models/logistic-regression.spec.ts
@@ -0,0 +1,24 @@
+import { test, expect } from '@playwright/test';
+import { login } from '../util/login';
+import { ModelFormData, predict } from '../util/predict';
+import { acceptCookies } from '../util/cookies';
+
+const formData: ModelFormData[] = [
+  { selector: '[name="X1"]', value: '1', type: 'input' },
+  { selector: '[name="X2"]', value: '1', type: 'input' },
+];
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://app.jaqpot.org/dashboard/models/1859/description');
+  await acceptCookies(page);
+});
+
+test('should allow prediction if logged in', async ({ page }) => {
+  await login(page);
+  await page.getByRole('tab', { name: 'Predict' }).click();
+
+  await predict(page, formData);
+
+  await expect(page.getByText('Success')).toBeVisible();
+  await expect(page.getByText('Probabilities')).toBeVisible();
+});
