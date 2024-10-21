8a018f355c14c03deb68ca52bc0bb4c8e2663afa
Alex Arvanitidis <alex_arvanitidis@mail.ntua.gr>
2024-10-21 15:50:21 +0300

    test: create e2e test for legacy model

---
+++ tests/legacy-models/redirect-works-properly.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+import { login } from '../util/login';
+import { ModelFormData, predict } from '../util/predict';
+import { acceptCookies } from '../util/cookies';
+
+const formData: ModelFormData[] = [
+  { selector: '[name="BW"]', value: '1', type: 'input' },
+  { selector: '[name="admin.type"]', value: 'IV', type: 'select' },
+  { selector: '[name="admin.time"]', value: '1', type: 'input' },
+  { selector: '[name="admin.dose"]', value: '1', type: 'input' },
+  { selector: '[name="sim.start"]', value: '1', type: 'input' },
+  { selector: '[name="sim.end"]', value: '11', type: 'input' },
+  { selector: '[name="sim.step"]', value: '1', type: 'input' },
+];
+
+test.beforeEach(async ({ page }) => {
+  const legacyUrl = 'https://app.jaqpot.org/model/gwNU9kWh3miIRwwYWLcn';
+  await page.goto(legacyUrl);
+  await acceptCookies(page);
+  await page.waitForURL('**/models/1730/description');
+});
+
+test('should allow prediction if logged in', async ({ page }) => {
+  await login(page);
+  await page.getByRole('tab', { name: 'Predict' }).click();
+
+  await predict(page, formData);
+
+  await expect(page.getByText('Success')).toBeVisible();
+});
