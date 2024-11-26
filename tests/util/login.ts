12d609476f8a96130495b42142480ddc5c0b4646
alarv <alarvfm@gmail.com>
2024-11-26 10:46:13 +0200

    fix: add timeout on login to avoid csrf cookies being sent simultaneously

--- tests/util/login.ts
+++ tests/util/login.ts
@@ -5,6 +5,8 @@ export const login = async (
   username: string = process.env.TEST_USER_USERNAME!!,
   password: string = process.env.TEST_USER_PASSWORD!!,
 ) => {
+  await page.waitForLoadState();
+  await page.waitForTimeout(1000);
   await page.getByRole('button', { name: 'Sign in' }).click();
 
   await page.waitForURL(
@@ -13,4 +15,19 @@ export const login = async (
   await page.locator('[name="username"]').fill(username);
   await page.locator('[name="password"]').fill(password);
   await page.getByRole('button', { name: 'Sign in' }).click();
+  await page.waitForURL('https://app.jaqpot.org/**');
+};
+
+export const logout = async (page: Page) => {
+  await page.waitForTimeout(1000);
+  await page.waitForLoadState();
+
+  await page.getByRole('button', { name: 'jaqpot e2e-' }).click();
+  await page.getByText('Sign out').click();
+  await page.waitForURL(
+    'https://login.jaqpot.org/realms/jaqpot/protocol/openid-connect/**',
+  );
+  await page.getByRole('button', { name: 'Logout' }).click();
+
+  await page.waitForURL('https://app.jaqpot.org/**');
 };
